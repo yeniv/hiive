d4f2f94658d7fa4ca523ec65c06046509af097b1
Max Teunissen <max.teunissen@mac.com>
2019-05-28 11:39:03 +0200

    added the products and profile controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'profile/show'
+  get 'products/index'
+  get 'products/new'
+  get 'products/create'
+  get 'products/show'
   devise_for :users
 
   root to: 'pages#home'
