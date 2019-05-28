c71cb264d108f35c29ad224e61ce016d45f0816f
Joao Lobo <joao.lobom@gmail.com>
2019-05-28 13:51:59 +0100

    with return method

--- test/regex.rb
+++ test/regex.rb
@@ -2,5 +2,5 @@ def validator(text)
   text.gsub(/[",']/, "")
   splitted = text.split(" ")
   arr = splitted.reject { |word| !word.match?(/(https?:)?(www\.)?([a-zA-Z0-9_%]*)\b\.[a-z]{2,4}(\.[a-z]{2})?((.[a-zA-Z0-9_%]*)+)?(\.[a-z]*)?/) }
-  p arr
+  return arr
 end
