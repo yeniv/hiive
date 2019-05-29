398ba4cc1b5b29bc8c5125420402573bb2b05c87
Tristan Viney <yenivyeniv@gmail.com>
2019-05-29 12:06:10 +0200

    updated price type from integer to string in products table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_28_121649) do
+ActiveRecord::Schema.define(version: 2019_05_29_095812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,7 +43,7 @@ ActiveRecord::Schema.define(version: 2019_05_28_121649) do
     t.bigint "user_id"
     t.string "referal_link"
     t.string "title"
-    t.integer "price"
+    t.string "price"
     t.text "description"
     t.string "brand"
     t.string "seller"
