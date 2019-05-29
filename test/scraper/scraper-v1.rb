b80149453295176e16b2e00f6f2373344e87e3ca
Tristan Viney <yenivyeniv@gmail.com>
2019-05-29 10:17:15 +0200

    added rescue to scraper

--- test/scraper/scraper-v1.rb
+++ test/scraper/scraper-v1.rb
@@ -14,25 +14,31 @@ def validator(text)
 end
 
 def amazon_scraper(url)
-  product_params = {}
-  html_file = RestClient.get(url)
-  html_doc = Nokogiri::HTML(html_file)
+  begin
+    product_params = {}
+    html_file = RestClient.get(url)
+    html_doc = Nokogiri::HTML(html_file)
 
-  product_params[:referal_link] = url
-  product_params[:title] = html_doc.search('#title').text.strip
-  product_params[:price] = html_doc.search('#price_inside_buybox').text.strip
-  product_params[:brand] = html_doc.search('#bylineInfo').text.strip
-  product_params[:seller] = "Amazon"
+    product_params[:referal_link] = url
+    product_params[:title] = html_doc.search('#title').text.strip
+    product_params[:price] = html_doc.search('#price_inside_buybox').text.strip
+    product_params[:brand] = html_doc.search('#bylineInfo').text.strip
+    product_params[:seller] = "Amazon"
 
-  description = []
-  html_doc.search('#feature-bullets script ~ li .a-list-item').each { |item| description << item.text.strip }
-  product_params[:description] = description.join("\n")
+    description = []
+    html_doc.search('#feature-bullets script ~ li .a-list-item').each { |item| description << item.text.strip }
+    product_params[:description] = description.join("\n")
 
-  images = html_doc.search('#imgTagWrapperId img').attr('data-a-dynamic-image').text
-  images_hash = JSON.parse(images.gsub('=>', ':'))
-  product_params[:photo] = images_hash.map { |key, value| key }
+    images = html_doc.search('#imgTagWrapperId img').attr('data-a-dynamic-image').text
+    images_hash = JSON.parse(images.gsub('=>', ':'))
+    product_params[:photo] = images_hash.map { |key, value| key }
 
-  product_params
+    product_params
+
+  rescue => error
+    puts error.inspect
+  end
+
 end
 
 def scrape(text)
@@ -40,11 +46,11 @@ def scrape(text)
   validator(text).each do |link|
     seller = link.match(url_regex)[2]
     case seller
-    when 'amzn' || 'amazon'
-      p amazon_scraper(link)
-      sleep 1
-    else
-      p "Sorry. Unable to auto-generate a link from #{seller}."
+      when 'amzn' || 'amazon'
+        p amazon_scraper(link)
+        # sleep 1
+      else
+        p "Sorry. Unable to auto-generate a link from #{seller}."
     end
   end
 end
