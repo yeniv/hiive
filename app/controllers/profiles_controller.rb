bdadec12af2d61a0b0a61b64aea174f65bfff5f8
Tristan Viney <yenivyeniv@gmail.com>
2019-05-29 14:53:36 +0200

    very basic homepage search working

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -4,10 +4,25 @@ class ProfilesController < ApplicationController
   def private_profile
     @user = current_user
     authorize :profile, :private_profile?
+
+    unless params[:link].nil?
+      product_params = Scraper.go(params[:link])
+
+      product_params.each do |params|
+        new_product = Product.new(params)
+        new_product.remote_photo_url = params[:photo]
+        new_product.user = @user
+        new_product.save!
+      end
+    end
+
+    @products = Product.where(user: @user)
   end
 
   def public_profile
     @user = User.find(params[:id])
     authorize :profile, :public_profile?
+
+    @products = Product.where(user: @user)
   end
 end
