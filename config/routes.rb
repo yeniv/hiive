3195a99fb9744c4b1a2158abdbb246ee348b42f4
maxsalemans <mhrsalemans@gmail.com>
2019-05-31 15:01:16 +0200

    new route to creators

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   end
 
   get '/discover', to: 'pages#discover'
+  get '/profiles', to: 'profiles#profiles', as: :creators
   get '/profile', to: 'profiles#private_profile', as: :private_profile
   get '/profile/:id', to: 'profiles#public_profile', as: :public_profile
 end
