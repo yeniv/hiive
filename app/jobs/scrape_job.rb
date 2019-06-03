60fbc8400ed96fdba11a11423feac1b82c40e5d0
Tristan Viney <yenivyeniv@gmail.com>
2019-06-03 14:54:57 +0200

    background tasks working, along with short and long amazon urls

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -4,10 +4,16 @@ class ScrapeJob < ApplicationJob
   def perform(link, user)
     url_regex = /(www\.)?([a-zA-Z0-9_%]*)\b\.[a-z]{2,4}(\.[a-z]{2})?/
     seller = link.match(url_regex)[2]
-    case seller
-    when 'amzn' || 'amazon'
-      new_product = Product.new(Scraper.amazon_scraper(link))
+
+    amazon_check = ['amzn', 'amazon'].include?(seller)
+
+    if amazon_check
+      product_params = Scraper.amazon_scraper(link)
+      return if product_params.nil?
+
+      new_product = Product.new(product_params)
       new_product.user = user # current_user
+      new_product.remote_photo_url = product_params[:photo]
       new_product.save!
     else
       p "Sorry. Unable to auto-generate a link from #{seller}."
