76f4b8dd35b1b3d9040b9f111815a40be66c815e
Joao Lobo <joao.lobom@gmail.com>
2019-06-03 15:04:47 +0100

    almost done

---
+++ app/javascript/plugins/dynamic_lookup.js
@@ -0,0 +1,22 @@
+var lookups = document.getElementById('query')
+lookups.addEventListener("keyup",function() {
+    
+    var input, filter, cards, cardContainer, title, i;
+    input = document.getElementById("myFilter");
+    filter = input.value.toUpperCase();
+    cardContainer = document.getElementById("myItems");
+    cards = cardContainer.getElementsByClassName("card-product");
+    for (i = 0; i < cards.length; i++) {
+        
+        title = cards[i].querySelector(".lookup");
+        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+            cards[i].style.display = "";
+        } else {
+            cards[i].style.display = "none";
+        }
+    }
+    console.log("hellddo")
+}
+)
+
+
