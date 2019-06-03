eed217dae2b2e030f53be7083aa0d801e55eab2a
Joao Lobo <joao.lobom@gmail.com>
2019-06-03 15:12:51 +0100

    finished

--- app/javascript/plugins/dynamic_lookup.js
+++ app/javascript/plugins/dynamic_lookup.js
@@ -1,22 +1,17 @@
-var lookups = document.getElementById('query')
-lookups.addEventListener("keyup",function() {
-    
-    var input, filter, cards, cardContainer, title, i;
-    input = document.getElementById("myFilter");
-    filter = input.value.toUpperCase();
-    cardContainer = document.getElementById("myItems");
-    cards = cardContainer.getElementsByClassName("card-product");
-    for (i = 0; i < cards.length; i++) {
-        
-        title = cards[i].querySelector(".lookup");
-        if (title.innerText.toUpperCase().indexOf(filter) > -1) {
-            cards[i].style.display = "";
-        } else {
-            cards[i].style.display = "none";
-        }
+var lookups = document.getElementById("query");
+lookups.addEventListener("keyup", function() {
+  var input, filter, cards, cardContainer, title, i;
+  input = document.getElementById("query");
+  filter = input.value.toUpperCase();
+  cardContainer = document.getElementById("myItems");
+  cards = cardContainer.getElementsByClassName("card-product");
+  for (i = 0; i < cards.length; i++) {
+    title = cards[i].querySelector(".lookup");
+    if (title.innerText.toUpperCase().indexOf(filter) > -1) {
+      cards[i].style.display = "";
+    } else {
+      cards[i].style.display = "none";
     }
-    console.log("hellddo")
-}
-)
-
-
+  }
+  console.log("hellddo");
+});
