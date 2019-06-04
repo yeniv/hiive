db33c87611976716d7c91b0bb59d75419e6a99d9
Tristan Viney <yenivyeniv@gmail.com>
2019-06-04 15:31:37 +0200

    flash cards working -- except product add cards

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,7 +7,7 @@ class ProfilesController < ApplicationController
 
     @products = Product.where(user: @user)
 
-    flash[:notice] = "👋 Hi #{@user.first_name}, this is your private profile. You're the only one who has access to this page. Enjoy!" if @user.products.empty?
+    flash.now[:notice] = "👋 Hi #{@user.first_name}, this is your private profile. You're the only one who has access to this page. Enjoy!" if @user.products.empty?
   end
 
   def public_profile
