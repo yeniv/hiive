721e67deeb4dbc1fc2cd7ee38b058492a59c3dad
Tristan Viney <yenivyeniv@gmail.com>
2019-06-05 16:37:52 +0200

    wip

--- app/javascript/plugins/api_refresh.js
+++ app/javascript/plugins/api_refresh.js
@@ -8,7 +8,7 @@ const apiFetch = () => {
     card_container.innerHTML = "";
     data.forEach((result) => {
       const productCard = `
-          <div class="col-4 hiive-card-container">
+          <div class="col-3 hiive-card-container">
           <div class="hiive-card" id="${result.id}"">
 
           <div class="hiive-card-photo" style="background-image: url(${result.photo.url})";>
@@ -20,8 +20,8 @@ const apiFetch = () => {
           </div>
           </div>
 
-          <form class="button_to" method="get" action="/products/${result.id}"><input class="col-6 hiive-card-btn hiive-card-btn-left" type="submit" value="more info"></form>
-          <form class="button_to" method="post" action="/products/${result.id}"><input type="hidden" name="_method" value="delete"><input class="col-6 hiive-card-btn hiive-card-btn-right hiive-card-btn-delete" type="submit" value="remove"></form>
+          <form class="button_to" method="post" action="/products/${result.id}"><input type="hidden" name="_method" value="delete"><input class="col-6 hiive-card-btn hiive-card-btn-left hiive-card-btn-delete" type="submit" value="remove"></form>
+          <form class="button_to" method="get" action="/products/${result.id}"><input class="col-6 hiive-card-btn hiive-card-btn-right" type="submit" value="more info"></form>
           </div>`;
 
       card_container.insertAdjacentHTML("afterbegin", productCard);
